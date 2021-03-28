972def95897a9d9baae6eed47dfd350b3b8f6eac
SergKnyazev <serg.knyazev77@gmail.com>
2021-03-28 12:42:39 +0300

    add a date to the component

--- src/components/TimeTrack/TimeTrack.jsx
+++ src/components/TimeTrack/TimeTrack.jsx
@@ -1,13 +1,78 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './TimeTrack.scss';
 
 const TimeTrack = () => {
+  console.log('render TimeTrack...');
+
+  const [workDate, setWorkDate] = useState(0);
+
+  useEffect(() => {
+    const currentDate = new Date();
+    console.log(currentDate);
+
+    const workDateY = currentDate.getFullYear().toString();
+
+    let workDateM = '';
+    let month = currentDate.getMonth() + 1;
+    workDateM  = month < 10 ? '0' + month : '' + month;
+
+    let workDateD = '';
+    let day = currentDate.getDate();
+    workDateD  = day < 10 ? '0' + day : '' + day;
+
+    setWorkDate(+(workDateY + workDateM + workDateD));
+    console.log(+(workDateY + workDateM + workDateD));
+    console.log(typeof +(workDateY + workDateM + workDateD));
+
+  }, [setWorkDate]);
 
   return (
     <section className='timetrack'>
       <p>TimeTrack</p>
+      <h3>Текущая дата : {workDate}</h3>
+    </section>
+)
+}
+
+export default TimeTrack;
+
+/**
+ * import React, {useEffect, useState} from 'react';
+ import './Workload.scss';
+
+ const Workload = () => {
+  console.log('render Workload...');
+
+  const [workDate, setWorkDate] = useState(0);
+
+  useEffect(() => {
+    const currentDate = new Date();
+    console.log(currentDate);
+
+    const workDateY = currentDate.getFullYear().toString();
+
+    let workDateM = '';
+    let month = currentDate.getMonth() + 1;
+    workDateM  = month < 10 ? '0' + month : '' + month;
+
+    let workDateD = '';
+    let day = currentDate.getDate();
+    workDateD  = day < 10 ? '0' + day : '' + day;
+
+    setWorkDate(+(workDateY + workDateM + workDateD));
+    console.log(+(workDateY + workDateM + workDateD));
+    console.log(typeof +(workDateY + workDateM + workDateD));
+
+  }, [setWorkDate]);
+
+
+  return (
+    <section className='workload'>
+      <h1>Workload</h1>
+      <h3>Текущая дата : {workDate}</h3>
     </section>
   )
 }
 
-export default TimeTrack;
+ export default Workload;
+ * */
