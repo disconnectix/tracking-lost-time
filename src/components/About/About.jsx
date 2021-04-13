4bac5964fe18f7157e1c9b585e4f0376a8b719ba
SergKnyazev <59279467+SergKnyazev@users.noreply.github.com>
2021-04-13 16:27:25 +0300

    add new WorkSelect custom component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,17 +1,83 @@
  import React from 'react';
  import './About.scss';
- import WorksSelect from './WorksSelect.jsx';
+ // import WorksSelect from './WorksSelect.jsx';
+ import WorkSelect2 from './WorkSelect2';
 
- const About = () => {
+//  const About = () => {
+//
+//   return (
+//     <section className='about'>
+//       <p>About</p>
+//
+//       <WorksSelect />
+//       <div>*************************************************************************************</div>
+//       <WorkSelect2 />
+//
+//     </section>
+//   )
+// }
 
-  return (
-    <section className='about'>
-      <p>About</p>
 
-      <WorksSelect />
 
-    </section>
-  )
+
+
+  class About extends React.Component {
+
+    countryList = [
+      { id: 1, name: "Australia", work: 'eat', bgColor: '#ff0000' },
+      { id: 2, name: "Brazil", work: 'splite', bgColor: '#00ff00' },
+      { id: 3, name: "China", work: 'listen', bgColor: '#0000ff' },
+      { id: 4, name: "Denmark", work: 'id=4 eatsplite', bgColor: '#ff00ff' },
+      { id: 5, name: "Egypt", work: 'eatlisten', bgColor: '#ffff00' },
+      { id: 6, name: "Finland", work: 'eateat', bgColor: '#0000ff' },
+      { id: 7, name: "Ghana", work: 'splitesplite', bgColor: '#00ffff' },
+      { id: 8, name: "Hungary", work: '123123splite', bgColor: '#fffff0' },
+      { id: 9, name: "India", work: '44444listen', bgColor: '#f0000f' },
+      { id: 10, name: "Japan", work: '55555eat', bgColor: '#f0f00f' }
+    ];
+
+    currentWork =  { id: 4, name: "Denmark", work: 'id=4 eatsplite', bgColor: '#ff00ff' };
+
+
+    constructor(props) {
+    super(props);
+
+    this.state = {
+      // showList: false,
+      selectText: "Please select an option",
+    };
+  }
+
+  //************************************************************** TEST TEST TEST
+    getCurrentWork = (data) => {
+      console.log(`work : ${data.work}`);
+      console.log(`bgColor : ${data.bgColor}`);
+  }
+  //************************************************************** TEST TEST TEST
+
+  render() {
+    return (
+      <div className="App">
+        <h3 className="title">Custom React Select</h3>
+        <div className="test">
+
+          <WorkSelect2
+            defaultText={this.state.selectText}
+            optionsList={this.countryList}
+            currentWork={this.currentWork}
+            getCurrentWork={this.getCurrentWork}
+          />
+
+        </div>
+      </div>
+    );
+  }
 }
 
+
+
+
+
+
  export default About;
+
---
+++ src/components/About/WorkSelect2.jsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import './WorkSelect2.scss';
+
+class WorkSelect2 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // @defaultSelectText => Show default text in select
+    // @showOptionList => Show / Hide List options
+    // @optionsList => List of options
+    this.state = {
+      selectText: '',
+      showOptionList: false,
+      // optionsList: []
+      work: '',
+      bgColor: '',
+    };
+  }
+
+  componentDidMount() {
+    // Add Event Listner to handle the click that happens outside
+    // the Custom Select Container
+    document.addEventListener('mousedown', this.handleClickOutside);
+    this.setState({
+      selectText: this.props.defaultText,
+
+      work: this.props.currentWork.work,
+      bgColor: this.props.currentWork.bgColor,
+    });
+  }
+
+  componentWillUnmount() {
+    // Remove the event listner on component unmounting
+    document.removeEventListener('mousedown', this.handleClickOutside);
+  }
+
+  // +++ This method handles the click that happens outside the
+  // select text and list area
+  handleClickOutside = e => {
+    if (
+      !e.target.classList.contains('select') &&
+      !e.target.classList.contains('select__current')
+    ) {
+      this.setState({
+        showOptionList: false
+      });
+    }
+  };
+
+  // +++ This method handles the display of option list
+  handleListDisplay = () => {
+    this.setState(prevState => {
+      return {
+        showOptionList: !prevState.showOptionList
+      };
+    });
+  };
+
+  // This method handles the setting of name in select text area
+  // and list display on selection
+  handleOptionClick = e => {
+    this.setState({
+      work: e.target.getAttribute('data-work'),
+      bgColor: e.target.getAttribute('data-bgcolor'),
+      showOptionList: false
+    });
+    //****************************************************** TEST TEST TEST
+    this.props.getCurrentWork({
+      work: e.target.getAttribute('data-work'),
+      bgColor: e.target.getAttribute('data-bgcolor'),
+    })
+  };
+
+  render() {
+    const { optionsList } = this.props;
+    const { showOptionList, work, bgColor } = this.state;
+
+    return (
+      <div className='select'>
+        <div
+          className={showOptionList ? 'select__current active' : 'select__current'}
+          style={{backgroundColor: bgColor}}
+          onClick={this.handleListDisplay}
+        >
+          {work}
+        </div>
+        {showOptionList && (
+          <ul className='select__options'>
+            {optionsList.map(option => {
+                return (
+                  <li
+                    className='select select__item'
+                    data-work={option.work}
+                    data-bgcolor={option.bgColor}
+                    key={option.id}
+                    onClick={this.handleOptionClick}
+                    style={{backgroundColor: option.bgColor}}
+                  >
+                    {option.work}
+                  </li>
+                );
+              })
+            }
+          </ul>
+        )}
+      </div>
+    );
+  }
+}
+
+export default WorkSelect2;
